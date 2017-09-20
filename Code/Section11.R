b96f425ca74c4bc5cf8fd93cf89f29c9f542f3bb
Emma Fox <eafox@g.ucla.edu>
2017-09-20 15:46:54 -0700

    Assignment Finished

--- Code/Section11.R
+++ Code/Section11.R
@@ -13,22 +13,22 @@ X <- read.csv("ChlorellaGrowth.csv",comment.char='#')
 Light <- X[,1]
 rmax <- X[,2]
 
-#11.0.0.0.1
+#11.1.0.0.1
 # See Mdfd_Intro2.R
 
-#11.0.0.0.2
+#11.1.0.0.2
 # Entering plot(fit) returns a series of graphs showing the distribution of error or residuals showing how normally distributed the data is and how much they affect the data.
 
-#11.0.0.0.3
+#11.1.0.0.3
 # Code beow has been copied from Intro2.R with modification for aixs values
 par(cex=1.5,cex.main=0.9)
 plot(rmax~Light,data=X,xlab="Log light intensity (uE/m2/s)",ylab="Maximum growth rate (1/d)",pch=16,xlim=c(0,120),ylim=c(1,4)) 
 title(main="Data from Fussmann et al. (2000) system")
 
-#11.0.0.0.4
+#11.1.0.0.4
 # See Mdfd2_Intro2.R
  
-#11.0.0.0.5
+#11.1.0.0.5
 par(mfrow=c(2,2))
 par(lty=1)
 plot(1, xlim=c(3,8),ylim=c(15,45)) ; abline(a=3,b=5,col="red")
@@ -39,8 +39,8 @@ plot(1, xlim=c(3,8),ylim=c(15,45)) ; abline(a=3,b=5,col="green")
 par(lty=6)
 plot(1, xlim=c(3,8),ylim=c(15,45)) ; abline(a=3,b=5,col="orange")
 
-#11.0.0.0.6
+#11.1.0.0.6
 dev.off()
 plot(rmax~Light,data=X,xlab="Log light intensity (uE/m2/s)",ylab="Maximum growth rate (1/d)",pch=16,xlim=c(0,120),ylim=c(1,4)) 
 title(main="Data from Fussmann et al. (2000) system")
-dev.print(pdf,"../Results/11.0.0.0.6.pdf")
+dev.print(pdf,"../Results/11.1.0.0.6.pdf")
--- Code/Section12.R
+++ Code/Section12.R
@@ -46,8 +46,30 @@ plot(T,N)
 # When T is empty (or has a length of 0) an error is plotted instead of an empty graph because T has no length
 # and vectors of different lengths cannot be used to plot x and y coordinates
 
-#12.0.0.0
+#12.2.0.0.1
+phi <- 20
+conv <- FALSE
+i<-0
+while (!conv) {
+  phi.new <- 1+1/phi
+  conv <- phi==phi.new
+  phi <- phi.new
+  i<-i+1
+}
+# It takes 40 iterations for phi to become constant
 
+#12.2.0.0.2
+a <- 1.1
+b <- 0.001
+n<-1
+while (TRUE) {
+  n.new <- a*n/(1+b*n)
+  if (n==n.new) break
+  n <- n.new
+}
+print(n)
+N[200]
+# n in this case is equal to the value the computations above converged at for Beverton-Holt
 
 
 
